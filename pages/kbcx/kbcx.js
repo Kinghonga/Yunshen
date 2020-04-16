ce889a44f735188b42b1d6c7e8301edb114790cc
qjh <1443164432@qq.com>
2020-04-17 01:52:40 +0800

    update the kbcx file

--- pages/kbcx/kbcx.js
+++ pages/kbcx/kbcx.js
@@ -3,39 +3,36 @@
 var app = getApp()
 Page({
   data: {
-    colorArrays: ["#85B8CF", "#90C652", "#D8AA5A", "#FC9F9D", "#0A9A84", "#61BC69", "#12AEF3", "#E29AAD"],
+    // 颜色数组，用于课程的背景颜色
+    colorArrays:["#85B8CF","#90C652","#D8AA5A","#FC9F9D","#0A9A84","#61BC69","#12AEF3","#E29AAD"],
+    // 课表数组
     wlist: [
-      { "xqj": 1, "skjc": 1, "kcmc": "计算机科学与技术","address":"A1-105"},
-      { "xqj": 1, "skjc": 5, "kcmc": "毛泽东思想邓小平理论实践课程","address": "A1-105"},
-      { "xqj": 2, "skjc": 1, "kcmc": "算法思想与课程设计", "address": "A1-105" },
-      { "xqj": 2, "skjc": 4, "kcmc": "高等数学", "address": "A1-105"},
-      { "xqj": 3, "skjc": 2, "kcmc": "大学英语", "address": "A1-105"},
-      { "xqj": 3, "skjc": 4, "kcmc": "篮球", "address": "体育馆1楼-B区"},
-      { "xqj": 3, "skjc": 3, "kcmc": "电子工艺实习", "address": "A1-105"},
-      { "xqj": 4, "skjc": 1, "kcmc": "计算机网络", "address": "A1-105"},
-      { "xqj": 4, "skjc": 4, "kcmc": "JavaEE课程设计", "address": "A1-105" },
-      { "xqj": 5, "skjc": 3, "kcmc": "设计模式", "address": "A1-105" },
-      // { "xqj": 6, "skjc": 1,  "kcmc": "高等数学@教A-301","address":"A1-105" },
-      // { "xqj": 7, "skjc": 2,  "kcmc": "高等数学@教A-301" ,"address":"A1-105"},
+      { "weekTime": 1, "jieCi": 1, "courseName": "计算机科学与技术","address":"A1-105"},
+      { "weekTime": 1, "jieCi": 5, "courseName": "毛泽东思想邓小平理论实践课程","address": "A1-105"},
+      { "weekTime": 2, "jieCi": 1, "courseName": "算法思想与课程设计", "address": "A1-105" },
+      { "weekTime": 2, "jieCi": 4, "courseName": "高等数学", "address": "A1-105"},
+      { "weekTime": 3, "jieCi": 2, "courseName": "大学英语", "address": "A1-105"},
+      { "weekTime": 3, "jieCi": 4, "courseName": "篮球", "address": "体育馆1楼-B区"},
+      { "weekTime": 3, "jieCi": 3, "courseName": "电子工艺实习", "address": "A1-105"},
+      { "weekTime": 4, "jieCi": 1, "courseName": "计算机网络", "address": "A1-105"},
+      { "weekTime": 4, "jieCi": 4, "courseName": "JavaEE课程设计", "address": "A1-105" },
+      { "weekTime": 5, "jieCi": 3, "courseName": "设计模式", "address": "A1-105" },
     ],
       kblist:[]
   },
   onLoad: function () {
-    console.log("发起请求")
-    var that = this;
-    wx.request({
-      url: 'https://127.0.0.1:8083/jx_getKb',
-      data: { id: 1 },
-      success(res) {
-        console.log(res)
-        that.setData({
-          kblist:res.data
-        });
+    // console.log("发起请求")
+    // var that = this;
+    // wx.request({
+    //   url: 'https://127.0.0.1:8083/jx_getKb',
+    //   data: { id: 1 },
+    //   success(res) {
+    //     console.log(res)
+    //     that.setData({
+    //       kblist:res.data
+    //     });
+    //   }
+    // })
 
-        
-      }
-    })
-
-  
   }
 })
